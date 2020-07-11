e552d08d262346a44e9626ea2b235abffab2a410
saddamhr <saddambubt65@gmail.com>
2020-07-11 22:04:18 +0600

    module 4 done

--- src/App.js
+++ src/App.js
@@ -5,28 +5,36 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'erty1', name: 'Max', age: 28 },
+      { id: 'dfwe1', name: 'Manu', age: 29 },
+      { id: 'gfchv1', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max!', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons})
+    this.setState({ persons: persons })
 
   }
 
@@ -34,6 +42,7 @@ class App extends Component {
     const doseShow = this.state.showPersons;
     this.setState({ showPersons: !doseShow });
   }
+
 
   render() {
     const style = {
@@ -54,7 +63,9 @@ class App extends Component {
               <Person
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
-                age={person.age} />
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
             )
           })}
         </div>
